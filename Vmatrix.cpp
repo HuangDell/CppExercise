fed18049c79a98e70dec8e4651e87c0f50ae5770
HuangDell <2429368752@qq.com>
2021-03-03 09:35:07 +0800

    Vmatrix --Decimal to hex self version

--- Vmatrix.cpp
+++ Vmatrix.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
-class Main{
-    int main(void)
+using namespace std;
+char *convertDecimalToHex(int);
+void convertDecimalToHex(int,char*);
+int main(void)
+{
+    int num,sum=0;
+    char *s;
+    std::cin>> num;
+    cout<< convertDecimalToHex(num);
+    convertDecimalToHex(num,s);
+    cout<< s<<endl;
+}
+void convertDecimalToHex(int value, char *s)
+{
+    s=convertDecimalToHex(value);
+}
+char * convertDecimalToHex(int value)
+{
+    int mod=0,index=48;
+    char hex[50]={0};
+    while(value)
     {
-        std::cout<< "HelloWorld"<< std::endl;
-        return 0;
+        mod=value%16;
+        switch (mod)
+        {
+            case 10:
+            hex[index]='a';
+            break;
+            case 11:
+            hex[index]='b';
+            case 12:
+            hex[index]='c';
+            break;
+            case 13:
+            hex[index]='d';
+            break;
+            case 14:
+            hex[index]='e';
+            break;
+            case 15:
+            hex[index]='f';
+            break;
+            default:
+            hex[index]='0'+mod;
+            break;
+        }
+        value/=16;
+        index--;
     }
-};
+    return hex+index+1;
+}
