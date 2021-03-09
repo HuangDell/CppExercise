917d65e0e02c263af944504214e5d618bacfbba1
HuangDell <2429368752@qq.com>
2021-03-09 12:35:18 +0800

    LeetCode 1047 AC--Self

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,43 +1,29 @@
-#include <vector>
 #include <iostream>
-#include <cstring>
+#include <string.h>
 using namespace std;
-class Solution
-{
+class Solution {
 public:
-    vector<int> an;
-    void getList(vector<vector <int>>& ve,vector<int>&num,int floors,int *hash)
-    {
-        if(floors==num.size()+1)
+    string removeDuplicates(string S) {
+        auto head=S.begin();
+        auto end=S.end();
+        while(head!=end-1 && head!=end)
         {
-            ve.push_back(an);
-            return ;
-        }
-        int size=num.size();
-        for (int i=0;i<size;i++)
-        {
-            if(hash[i]==0)
+            if(*head==*(head+1))
             {
-                hash[i]=1;
-                an.push_back(num[i]);
-                getList(ve,num,floors+1,hash);
-                hash[i]=0;
-                an.pop_back();
+                S.erase(head);
+                S.erase(head);
+                return removeDuplicates(S);
             }
+            else 
+            head++;
         }
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        int hash[nums.size()];
-        memset(hash,0,sizeof(int)*nums.size());
-        vector<vector<int>> ve;
-        getList(ve,nums,1,hash);
-        return ve;
-        
+        return S;
     }
 };
 
 int main(void)
 {
-    vector <int> nums{1,2,1};
     Solution s=Solution();
+    cout << s.removeDuplicates("aaaaaaaa");
+
 }
